052fe61a406947041d75ff8072af05e333d9ee07
nikhartman <nik.hartman@gmail.com>
2017-02-06 11:17:23 -0800

    style. remove qweb dependence in igh.py

--- IGH.py
+++ IGH.py
@@ -48,7 +48,7 @@ class IGH:
 	
 	# ATTENTION:	Don't use this when several ISOBUS devices are connected and powered on
 	# ALSO: 		Don't forget to save the changes into the permanent memory after running this function
-	def SetIsobusAddress(self, address):
+	def setIsobusAddress(self, address):
 		response = ''
 		#if qweb.lock(self.port_id, self.owner) == '100':
 		#try:
@@ -69,15 +69,13 @@ class IGH:
 			ch = self.serial_port.read().decode('utf-8')
 			response += ch
 		self.serial_port.flush()		
-
 		
 		self.machine_id = address
 		return response
 		
-	def RunCommand(self, cmd):
+	def runCommand(self, cmd):
 		response = ''
-		#if qweb.lock(self.port_id, self.owner) == '100':
-		#try:
+
 		to_write = '@{:d}{}{}'.format(self.machine_id, cmd, EOL)
 		if not self.serial_port.isOpen():
 			self.serial_port.open()
@@ -86,7 +84,7 @@ class IGH:
 		
 		wrote = self.serial_port.write(to_write.encode('latin1'))
 		if wrote != len(to_write):
-			print('Nope. Command didn\'t work: ', cmd)
+			print('COMMAND FAILED: ', cmd)
 		
 
 		t = time.time()			# Don't try to read forever, if there's nothing
@@ -102,8 +100,8 @@ class IGH:
 			print('Error: ', response)
 		return response
 
-	def GetStatus(self):
-		output = self.RunCommand('X')
+	def getStatus(self):
+		output = self.runCommand('X')
 		X = int(output[1:2])
 		A = int(output[3:4])
 		C = int(output[5:6])
@@ -147,138 +145,136 @@ class IGH:
 		return output
 
 	# This will return the Sorb Temperature if the current isobus machine_id corresponds to an IGH
-	def GetSorbTemp(self):
-		output = self.RunCommand('R1')
+	def getSorbTemp(self):
+		output = self.runCommand('R1')
 		self.SorbTemp = float(output[3:])/10
 		return self.SorbTemp
 
 	# This will return the 1K Pot Temperature if the current isobus machine_id corresponds to an IGH
-	def GetOneKPotTemp(self):
-		output = self.RunCommand('R2')
+	def getOneKPotTemp(self):
+		output = self.runCommand('R2')
 		self.OneKPotTemp = float(output[3:])/1000
 		return self.OneKPotTemp
 
-	def GetMixChTemp(self):
-		output = self.RunCommand('R3')
+	def getMixChTemp(self):
+		output = self.runCommand('R3')
 		val = float(output[3:])
 		if val < 3000:
-			output = self.RunCommand('R32')
+			output = self.runCommand('R32')
 			val = float(output[2:])/10
 		
 		self.MixChTemp = val
 		return val
 		
-	def GetMixChPower(self):
+	def getMixChPower(self):
 		'''
 		Mixing chamber power in units of micro Watts
 		'''
-		self.GetStatus()	
-		output = self.RunCommand('R4')
+		self.getStatus()	
+		output = self.runCommand('R4')
 		r = pow(10,self.MixPowerRange - 4)
 		self.MixChPower = float(output[3:]) * r / 1000
 		return float(float(output[3:]) * r)
 		
-	def GetStillPower(self):
-		output = self.RunCommand('R5')
+	def getStillPower(self):
+		output = self.runCommand('R5')
 		self.StillPower = float(output[3:])/10
 		return self.StillPower
 	
-	def GetSorbPower(self):
-		output = self.RunCommand('R6')
+	def getSorbPower(self):
+		output = self.runCommand('R6')
 		self.SorbPower = float(output[3:])
 		return self.SorbPower
 
-	def GetV6(self):
-		output = self.RunCommand('R7')
+	def getV6(self):
+		output = self.runCommand('R7')
 		self.V6 = float(output[2:])/10
 		return self.V6
 		
-	def GetV12A(self):
-		output = self.RunCommand('R8')
+	def getV12A(self):
+		output = self.runCommand('R8')
 		self.V12A = float(output[2:])/10
 		return self.V12A
 
-	def GetNV(self):
-		output = self.RunCommand('R9')
+	def getNV(self):
+		output = self.runCommand('R9')
 		self.NV = float(output[2:])/10
 		return self.NV
 	
-	def GetG1(self):
-		output = self.RunCommand('R14')
+	def retG1(self):
+		output = self.runCommand('R14')
 		self.G1 = float(output[3:])/10
 		return self.G1
 
-	def GetG2(self):
-		output = self.RunCommand('R15')
+	def getG2(self):
+		output = self.runCommand('R15')
 		self.G2 = float(output[3:])/10
 		return self.G2
 
-	def GetG3(self):
-		output = self.RunCommand('R16')
+	def getG3(self):
+		output = self.runCommand('R16')
 		self.G3 = float(output[3:])/10
 		return self.G3
 
-	def GetP1(self):
-		output = self.RunCommand('R20')
+	def getP1(self):
+		output = self.runCommand('R20')
 		self.P1 = float(output[1:])
 		return self.P1
 
-	def GetP2(self):
-		output = self.RunCommand('R21')
+	def getP2(self):
+		output = self.runCommand('R21')
 		self.P2 = float(output[1:])
 		return self.P2
 
-	def GetMixChResistance(self):
-		output = self.RunCommand('R35')
+	def getMixChResistance(self):
+		output = self.runCommand('R35')
 		self.MixChResistance = float(output[1:])*100
 		return self.MixChResistance
 	
 	# This will return the helium level if the current isobus machine_id corresponds to an ILM
-	def GetHeliumLevel(self):
-		output = self.RunCommand('R1')
+	def getHeliumLevel(self):
+		output = self.runCommand('R1')
 		self.HeliumLevel = float(output[3:])/10
 		return self.HeliumLevel
 
 	# This will return the nitrogen level if the current isobus machine_id corresponds to an ILM		
-	def GetNitrogenLevel(self):
-		output = self.RunCommand('R2')
+	def getNitrogenLevel(self):
+		output = self.runCommand('R2')
 		self.NitrogenLevel = float(output[3:])/10
 		return self.NitrogenLevel
-		
+	
+	def setRemote(self):
+		self.runCommand('C3')
 
-	def SetRemote(self):
-		self.RunCommand('C3')
-
-	def SetLocal(self):
-		self.RunCommand('C2')
+	def setLocal(self):
+		self.runCommand('C2')
 
 	# ValveName='V1', "V2", "V1A"
 	# For valves 'NV', "V6" and "V12A", value means percent open (0 <= value <= 100).
 	# For other valves, value 1 = on, 0 = off
-	def SetValve(self, valve, value):
+	def setValve(self, valve, value):
 		# print 'Value: ', value, "AND", '%03d' % int(value*10), "AND", "N"+'%03d' % int(value*10), "Decimal:", Decimal(value*10)
 		try:
-			self.SetRemote()	
+			self.setRemote()	
 			if valve=='V6':
-				self.RunCommand('G'+round(value*10))
+				self.runCommand('G'+round(value*10))
 				self.V6 = value
 			elif valve=='V12A':
-				self.RunCommand('H'+round(value*10))
+				self.runCommand('H'+round(value*10))
 				self.V12A = value
 			elif valve=='NV':
 				sval = '%03d' % round(value*10)
 				print('sval: ', sval)
-				print('Command returned: ', self.RunCommand("N"+sval))
+				print('Command returned: ', self.runCommand("N"+sval))
 				
 		finally:
-			self.SetLocal()
+			self.setLocal()
 
-
-	def AdjustNV(self):
-		step = float(qweb.getConfig('ighn_nv_step'))
-		P2lo = float(qweb.getConfig('ighn_p2nv_low'))
-		P2hi = float(qweb.getConfig('ighn_p2nv_high'))
-		self.GetNV()
+	def adjustNV(self, step, P2lo, P2hi):
+# 		step = float(qweb.getConfig('ighn_nv_step'))
+# 		P2lo = float(qweb.getConfig('ighn_p2nv_low'))
+# 		P2hi = float(qweb.getConfig('ighn_p2nv_high'))
+		self.getNV()
 		#print '==================== Adjust NV ======================'
 		#print time.strftime('%H:%M:%S'), "P2:", self.P2, "step:", step, "P2 Low:", P2lo, "P2 High:", P2hi, " NV:", self.NV		
 		
