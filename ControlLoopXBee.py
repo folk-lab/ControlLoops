03e1fc2c4447e3cb5099ea28860b1099e296a17d
nikhartman <nik.hartman@gmail.com>
2017-02-06 15:43:24 -0800

    moving on to xbee stuff

--- ControlLoopXBee.py
+++ ControlLoopXBee.py
@@ -6,31 +6,41 @@ import struct
 import qweb
 import binascii
 
-ser=serial.Serial('/dev/ttyUSB0', 19200)
-xbee = ZigBee(ser, escaped = True)
+# There are 2 XBee emitters in the lab
+#     XBee (1) -- by the purifier. reads helium bottle pressure and oxygen level after purifier
+#     XBee (2) -- mounted on the liquifier. reads liquefier level.
 
-def ReadXBee(loggable_name):
-	#print loggable_name
+# There is 1 XBee receiver in the lab.
+#     XBee (3) -- mounted on qdot-server 
+
+def readXBee(xb, loggable_name):
+
 	packet = None
+	
+	# figure out what we are going to read
 	if loggable_name=="oxygen_after_purifier":
-		XbeeId = b'\x00\x13\xA2\x00\x40\xA8\x2F\xB3'
+		XbeeId = b'\x00\x13\xA2\x00\x40\xA8\x2F\xB3' # XBee 1
 		cmd = b'ReadOxAftr'
 	elif loggable_name=="bottles_pressure":
-		XbeeId = b'\x00\x13\xA2\x00\x40\xA8\x2F\xB3'
+		XbeeId = b'\x00\x13\xA2\x00\x40\xA8\x2F\xB3' # XBee 1
 		cmd = b'ReadBotPrs'
 	elif loggable_name=="liquefier_level":
-		XbeeId = b'\x00\x13\xa2\x00\x40\xbf\x8f\x59'
+		XbeeId = b'\x00\x13\xa2\x00\x40\xbf\x8f\x59' # XBee 2
 		cmd = b'ReadLiq'
-	elif loggable_name=="purifier_nitrogen_level":
-		XbeeId = b'\x00\x13\xa2\x00\x40\xa0\x96\xb1'
-		cmd = b'ReadC'
+# 	elif loggable_name=="purifier_nitrogen_level":
+# 		XbeeId = b'\x00\x13\xa2\x00\x40\xa0\x96\xb1'
+# 		cmd = b'ReadC'
 	else:
-		raise Exception('I don\'t know what to do with this loggable_name.'+loggable_name)
+		raise KeyError('Unknown loggable_name.'+loggable_name)
 	
-	xbee.tx(frame=b'\x02', dest_addr=b'\xFF\xFE', dest_addr_long=XbeeId, data=cmd)
-	packet = xbee.wait_read_frame()
+	# transmit request for data
+	xb.tx(frame=b'\x02', dest_addr=b'\xFF\xFE', dest_addr_long=XbeeId, data=cmd)
+
+    # wait for response -- IS THIS THE PROBLEM?	
+    # this is really sloppy
+	packet = xb.wait_read_frame()
 	while not packet['id']=="rx":
-		packet = xbee.wait_read_frame()
+		packet = xb.wait_read_frame()
 	
 	d = packet['rf_data'].decode('utf-8')
 	if d.find("=") > -1:
@@ -41,7 +51,7 @@ def ReadXBee(loggable_name):
 	else:
 		print(packet)
 
-def log():
+def log(xb):
 	response = qweb.getLoggableInfoForNow('xbee')
 	sensors = str.split(response, "\n")
 	machine_type=""
@@ -54,27 +64,30 @@ def log():
 					loggable_name = keyvals[1]
 				elif keyvals[0]=="machine_type":
 					machine_type = keyvals[1]
-			if machine_type == "xbee":
+			if machine_type == "xbee": # what is the purpose of machine_type?
 				try:
-					ReadXBee(loggable_name)
+					readXBee(xb, loggable_name)
 				except Exception as e:
 					print(str(datetime.datetime.now()), ": ",e)
 
+#### Main Control Loop ####
 
-period_all = 15 #seconds
-t_all = 0
+if __name__ == "__main__":
 
-while True:
-	try:
-		if time.time() - t_all >= period_all:
-			log()
-			t_all = time.time()
+    period_all = 15 #seconds
+    t_all = 0
 
-		time.sleep(1)
-	except Exception as e:
-		print(str(datetime.datetime.now()), ": ", e)
+    ser=serial.Serial('/dev/ttyUSB0', 19200) # open serial port
+    xb = ZigBee(ser, escaped = True) # create zigbee object
 
-xbee.halt()
-ser.close()
+    while True:
+        try:
+            if time.time() - t_all >= period_all:
+                log(xb)
+                t_all = time.time()
+
+            time.sleep(2)
+        except Exception as e:
+            print(str(datetime.datetime.now()), ": ", e)
 
 
